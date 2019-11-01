57584393cec52e2f346939375d0efd411c7976d0
essential-robot-4364 <brandon.james414@gmail.com>
2019-11-01 13:02:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash)
+def key_for_min_value(ninja)
   smallest_key = nil
   tiniest_value = nil
-  hash.each do |key, value|
+  ninja.each do |key, value|
     if tiniest_value == nil || value < tiniest_value
       tiniest_value = value
       smallest_key = key
